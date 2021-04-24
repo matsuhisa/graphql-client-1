5fbd5f19ea7fe015b953fbf9f1216bd98e6cb438
matsuhisa <matsuhisa+github@gmail.com>
2021-04-24 23:53:03 +0900

    Add Mutation

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
+
 
 const ROOT_QUERY = gql`
   query allUsers {
@@ -11,16 +12,32 @@ const ROOT_QUERY = gql`
     }
   }
 `
+const ADD_FAKE_USERS_MUTATION = gql`
+mutation addFakeUsers($count:Int!) {
+  addFakeUsers(count: $count) {
+    githubLogin
+    name
+    avatar
+  }
+}
+`
 
 export const Users = () => {
   const { loading, error, data, refetch } = useQuery(ROOT_QUERY)
+  const [ addFakeUser ] = useMutation(ADD_FAKE_USERS_MUTATION)
+  let input: any
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>error...</p>
 
   return (
     <>
-      <button onClick={() => refetch()}>Refetch!</button>
+      <button onClick={ () => refetch() }>Refetch!</button>
+      <input ref={ node => { input = node } } />
+      <button onClick={ () => { 
+        addFakeUser({ variables: { count: Number(input.value) } })
+        input.value = 0
+      }}> Add FakeUser</button>
       <p>
         {data.allUsers.length}
       </p>
