0d2b368432487a34997a26165afc5d5de7e2e23c
matsuhisa <matsuhisa+github@gmail.com>
2021-05-01 12:36:14 +0900

    Add AuthorizedUser

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Users } from './Users'
 import { BrowserRouter } from 'react-router-dom'
+import AuthorizedUser from './AuthorizedUser'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <AuthorizedUser />
       <Users />
     </BrowserRouter>
   )
---
+++ src/AuthorizedUser.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { withRouter, RouteComponentProps } from 'react-router-dom'
+import { gql } from '@apollo/client'
+
+interface Props extends RouteComponentProps {}
+
+class AuthorizedUser extends React.Component<Props> {
+  state = { signingIn: false }
+
+  componentDidMount() {
+    if(window.location.search.match(/code=/)) {
+      this.setState({ signingIn: true })
+      const code = window.location.search.replace("?code=", "")
+      alert(code)
+      this.props.history.replace('/')
+    }
+  }
+
+  requestCode() {
+    var clientID = process.env.REACT_APP_GITHUB_CLIENT_ID
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`
+  }
+
+  render() {
+    return (
+      <button onClick={this.requestCode} disabled={this.state.signingIn}>
+        Sign In with Github
+      </button>
+    )
+  }
+}
+
+export default withRouter(AuthorizedUser)
